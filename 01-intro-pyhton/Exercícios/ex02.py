474b3dd67e2baa4caf90e4ff017364330b29ab2c
João O. Félix <126785897+jpfelixx@users.noreply.github.com>
2024-03-26 18:41:08 -0300

    Update ex02.py

--- 01-intro-pyhton/Exercícios/ex02.py
+++ 01-intro-pyhton/Exercícios/ex02.py
@@ -1,7 +1,7 @@
 #Escreva um programa que solicita ao usuário 3 valores e apresenta a média aritmética dos números digitados
-numero = float(input('digite o primeiro nro '))
-numero1 = float(input('digite o segundo nro '))
-numero2 = float(input('digite o terceiro nrmo '))
+numero = float(input('digite o primeiro nmro: '))
+numero1 = float(input('digite o segundo nmro: '))
+numero2 = float(input('digite o terceiro nmro: '))
 m  = (numero+numero1+numero2)/3;
 
 print(m)
