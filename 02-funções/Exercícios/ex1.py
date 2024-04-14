4490c336d4775bad4cfe8ff370c2565e88d3b07d
jpfelixx <felix.joao@aluno.ifsp.edu.br>
2024-04-14 17:46:53 -0300

    final

--- 02-funções/Exercícios/ex1.py
+++ 02-funções/Exercícios/ex1.py
@@ -1,7 +1,6 @@
 #Ex01 - Jogo de Adivinhação Peça ao usuário para adivinhar um número entre 1 e 100 que o programa escolheu
 #aleatoriamente. Informe ao usuário se o palpite está alto ou baixo, até que ele acerte o número.
 import random
-
 def jogo_advinhar(number, numero_aleatorio):
     if number == numero_aleatorio:
         return 1
--- 02-funções/Exercícios/ex7.py
+++ 02-funções/Exercícios/ex7.py
@@ -1,6 +1,5 @@
 #Ex07 -  Jogo da Forca: Implemente uma versão simples do jogo da forca. O programa começa com uma palavra oculta ( o usuário não vê ) e o 
 # usuário tenta adivinhar a palavra, letra por letra. O usuário tem um número limitado de tentativas para adivinhar toda a palavra.
-
 def entrada():
     opc = input("|Digite 1 para tentar adivinhar uma letra\n|Digite 2 para tentar adivinhar a Palavra\n: ").strip()
 
