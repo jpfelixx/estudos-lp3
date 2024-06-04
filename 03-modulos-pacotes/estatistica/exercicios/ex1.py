e49ccf9f26ab06b0dc9b607da5d4a0f67bbc8609
Estudante 1 <estudante1@debian.spo.ifsp.edu.br>
2024-06-04 08:57:44 -0300

    exer

---
+++ 03-modulos-pacotes/estatistica/exercicios/ex1.py
@@ -0,0 +1,13 @@
+'''
+Para cada um dos exercícios a seguir crie um arquivo ex01.py e ex02.py e resolva o exercício utilizando módulos e pacotes (se necessário). Ao final envie o link do seu repositório no GitHub nesta tarefa.
+
+Ex01. Crie um programa que recebe como entrada o comprimento, altura e a largura de um aquário e calcule as seguintes informações.
+
+    O volume do aquário em litros;
+    A potência do termostato necessária para manter a temperatura adequada dentro do aquário;
+    A quantidade em litros de filtragem por hora necessária para manter a qualidade da água.
+
+    Volume é dado por (comprimento * altura * largura) / 1000
+    A potência do termostato depende do tamanho do aquário e da temperatura ambiente. Para o cálculo utilizar a fórmula: potencia = volume * 0,05 * (temperatura desejada - temperatura ambiente)
+    A quantidade de filtragem por hora deve ser no mínimo 2 a 3 vezes o volume do aquário.
+'''
---
+++ 03-modulos-pacotes/estatistica/exercicios/ex2.py
@@ -0,0 +1,17 @@
+'''
+Ex02. Utilizando as diretrizes de Índice de Massa Corporal (IMC) da Organização Mundial de Saúde (OMS), crie uma calculadora que solicita ao usuário seu peso (Kg) e sua altura (m) e apresenta em qual classificação o indivíduo se encaixa. Além disso, o programa deve apresentar quanto o indivíduo precisa perder ou ganhar de peso para chegar no peso normal (imc = 24,9).
+
+IMC = peso / altura * altura
+
+Classificação
+----------------------------------
+IMC           Classificação
+-----------------------------------
+< 18,5             Baixo peso
+18,5 a 24,9     Peso normal
+25,0 a 29,9     Excesso de peso
+30,0 a 34,9     Obesidade de Classe 1
+35,0 a 39,9     Obesidade de Classe 2
+>= 40,00         Obesidade de Classe 3
+
+'''
